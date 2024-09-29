af1f265d2d438b5be1088d48c978ce7702728e4b
pprunty <pprunty@tcd.ie>
2024-09-29 22:06:28 +0100

    fix

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@
 export default function Footer() {
   return (
     <footer className="flex text-md flex-col items-center pb-4">
-     {/* <Image src="/images/line.svg" fill="currentColor" width="125" height="54" /> */}
+      {/* <Image src="/images/line.svg" fill="currentColor" width="125" height="54" /> */}
       <div className="flex justify-center mt-10">
         <a
           className="transition hover:underline"
@@ -13,37 +13,40 @@ export default function Footer() {
         </a>
         <span className="mx-2">·</span>
         <a
-          className="transition hidden md:inline-block hover:underline"
-          href="https://dribbble.com/phillzou"
+          className="hover:underline"
+          href="https://twitter.com/phillzou"
         >
-          Dribbble
-        </a>
-        <span className="mx-2 hidden md:inline-block">·</span>
-        <a className="hover:underline" href="https://twitter.com/phillzou">
           Twitter
         </a>
         <span className="mx-2">·</span>
-        <a className="transition hover:underline" href="https://www.buymeacoffee.com/">
+        <a
+          className="transition hover:underline"
+          href="https://www.buymeacoffee.com/"
+        >
           Buy me a coffee
+        </a>
+        <span className="mx-2">·</span>
+        <a
+          className="transition hover:underline"
+          href="https://www.strava.com/athletes/72636452"
+        >
+          Strava
         </a>
       </div>
       <div className="text-gray-500 mt-3 px-6 text-center">
+        Developed by{" "}
         <a
-          href="https://abstractlab.gumroad.com/l/noto-avatar"
+          href="https://twitter.com/felix12777"
           className="hover:underline"
         >
-          &copy; Illustrations
-        </a>
-        Designed by
-        <a href="https://twitter.com/felix12777" className="hover:underline">
-          Felix Wong
-        </a>
-        under design
+          Patrick Prunty
+        </a>{" "}
+        under{" "}
         <a
           className="hover:underline"
-          href="https://creativecommons.org/publicdomain/zero/1.0/"
+          href="https://opensource.org/license/mit"
         >
-          CC0 license
+          MIT license
         </a>
       </div>
     </footer>
