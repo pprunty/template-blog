4bdc0856bab3c93f54a540c4a21a1f6363ee3434
pprunty <pprunty@tcd.ie>
2024-10-08 11:27:56 +0100

    fix youtube

--- src/app/blog/components/youtube.tsx
+++ src/app/blog/components/youtube.tsx
@@ -1,31 +1,12 @@
 "use client";
-
-import styled from 'styled-components';
 import YT, { YouTubeProps } from 'react-youtube'; // Import YouTubeProps from react-youtube
 
-// Styled component for the YouTube container
-const StyledYouTubeContainer = styled.div`
-  width: 100%;
-  max-width: 100%;
-  margin: 1.25rem 0; /* Equivalent to Tailwind's my-5 */
-  position: relative;
-  overflow: hidden;
-  padding-top: 56.25%; /* Aspect ratio 16:9 (9/16 = 0.5625 or 56.25%) */
-
-  iframe {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-  }
-`;
-
-// YouTube component with proper typing
 export function YouTube(props: YouTubeProps) {
   return (
-    <StyledYouTubeContainer>
-      <YT {...props} />
-    </StyledYouTubeContainer>
+    <div className="w-full max-w-full my-5 relative overflow-hidden pt-[56.25%]">
+      <div className="absolute top-0 left-0 w-full h-full">
+        <YT {...props} />
+      </div>
+    </div>
   );
 }
