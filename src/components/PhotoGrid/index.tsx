8e7f73bb4d504a776ab5b25b87d60aa4fb53d6bb
pprunty <pprunty@tcd.ie>
2024-10-24 13:00:15 +0100

    updates

--- src/components/PhotoGrid/index.tsx
+++ src/components/PhotoGrid/index.tsx
@@ -2,7 +2,7 @@
 
 'use client';
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { MemoizedImage } from '@/components/MemoizedImage';
 
 interface PhotoGridProps {
@@ -10,9 +10,35 @@ interface PhotoGridProps {
 }
 
 const PhotoGrid: React.FC<PhotoGridProps> = ({ images }) => {
+  const [columns, setColumns] = useState(2); // Default to 2 columns
+
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+
+      if (width >= 1024) {
+        // lg breakpoint
+        setColumns(3);
+      } else {
+        setColumns(2);
+      }
+    };
+
+    // Initialize columns on component mount
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  // Calculate the number of full rows and adjust the images array
+  const fullRows = Math.floor(images.length / columns);
+  const numImagesToDisplay = fullRows * columns;
+  const imagesToDisplay = images.slice(0, numImagesToDisplay);
+
   return (
     <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-1 py-4">
-      {images.map((src, index) => (
+      {imagesToDisplay.map((src, index) => (
         <div key={index} className="relative aspect-square">
           <MemoizedImage
             src={src}
